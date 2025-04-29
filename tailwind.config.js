b1a43305a9efdefccf14a47f4f940e5f4cbca82d
Weaver17 <ajweaver18@gmail.com>
2025-04-29 17:19:47 -0400

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#260947",
+        secondary: "#ebe1f5",
+        light: {
+          100: "#7f4cba",
+          200: "#9e86ba",
+          300: "#5704b8",
+        },
+        dark: {
+          100: "#2f2140",
+          200: "#271240",
+        },
+        accent: "#ab72e8",
+      },
+    },
+  },
+  plugins: [],
+};
