096dc9c7184cb53dd862f9f54f1b45ebf8feae0c
Weaver17 <ajweaver18@gmail.com>
2025-05-02 14:09:34 -0400

    feat: trending complete & movie details page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import TrendingCard from "@/components/TrendingCard";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
@@ -70,6 +71,7 @@ export default function Index() {
                         {/* <SearchBar
                             onPress={() => router.push("/search")}
                             placeholder="Search for a Movie"
+                            value={v}
                         /> */}
 
                         {trendingMovies && (
@@ -82,14 +84,19 @@ export default function Index() {
                                     className="mb-4 mt-3"
                                     data={trendingMovies}
                                     renderItem={({ item, index }) => (
-                                        <Text className="text-white text-sm">
-                                            {item.title}
-                                        </Text>
+                                        <TrendingCard
+                                            movie={item}
+                                            index={index}
+                                        />
                                     )}
                                     keyExtractor={(item) =>
                                         item.movie_id.toString()
                                     }
                                     horizontal
+                                    showsHorizontalScrollIndicator={false}
+                                    ItemSeparatorComponent={() => (
+                                        <View className="w-4" />
+                                    )}
                                 />
                             </View>
                         )}
